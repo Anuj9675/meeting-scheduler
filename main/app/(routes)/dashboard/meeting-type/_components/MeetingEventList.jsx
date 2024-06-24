298a3c1054d02e3985987930dd57418f5e6e9c4a
Anuj9675 <anujupadhyay6265@gmail.com>
2024-06-24 18:49:52 +0530

    Changes in loading

--- main/app/(routes)/dashboard/meeting-type/_components/MeetingEventList.jsx
+++ main/app/(routes)/dashboard/meeting-type/_components/MeetingEventList.jsx
@@ -11,8 +11,6 @@ import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
     DropdownMenuTrigger,
   } from "@/components/ui/dropdown-menu"
   
@@ -99,11 +97,17 @@ function MeetingEventList() {
                     >
                         <Copy className='h-4 w-4'/> Copy Link </h2>
                     <Button variant="outline" 
-                    className="rounded-full text-primary border-primary ">Share</Button>
+                    className="rounded-full text-primary border-primary ">Share Via Copy</Button>
                     </div>
                 </div>
             ))
-                :<h2>Loading...</h2>
+                :<div
+                className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                role="status">
+                <span
+                  className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                >Loading...</span>
+              </div>
         }
         </div>
     )
