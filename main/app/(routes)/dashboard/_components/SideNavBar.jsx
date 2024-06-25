c09bdebfb86964c7fb90de9d7beb7ae49a88cbe7
Anuj9675 <anujupadhyay6265@gmail.com>
2024-06-25 10:38:45 +0530

    changes

--- main/app/(routes)/dashboard/_components/SideNavBar.jsx
+++ main/app/(routes)/dashboard/_components/SideNavBar.jsx
@@ -15,15 +15,20 @@ function SideNavBar() {
             path:'/dashboard/meeting-type',
             icon:Briefcase
         },
-
         {
             id:2,
+            name:'Scheduled Meeting',
+            path:'/dashboard/scheduled-meeting',
+            icon:Calendar
+        },
+        {
+            id:3,
             name:'Availability',
             path:'/dashboard/availability',
             icon:Clock
         },
         {
-            id:3,
+            id:4,
             name:'Settings',
             path:'/dashboard/settings',
             icon:Settings
