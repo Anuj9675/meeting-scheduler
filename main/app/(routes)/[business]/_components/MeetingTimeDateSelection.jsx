93841d5efa49aaa7f1325a4877a61537267d2ce7
Anuj9675 <anujupadhyay6265@gmail.com>
2024-06-26 11:32:07 +0530

    changes

--- main/app/(routes)/[business]/_components/MeetingTimeDateSelection.jsx
+++ main/app/(routes)/[business]/_components/MeetingTimeDateSelection.jsx
@@ -1,5 +1,6 @@
-"use client"
 import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import { format } from 'date-fns';
 import { CalendarCheck, Clock, LoaderIcon, MapPin, Timer } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -9,7 +10,7 @@ import UserFormInfo from './UserFormInfo';
 import { collection, doc, getDocs, getFirestore, query, setDoc, where } from 'firebase/firestore';
 import { app } from '@/config/FirebaseConfig';
 import { toast } from 'sonner';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import Plunk from '@plunk/node';
 import { render } from '@react-email/render';
 import Email from '@/emails';
@@ -18,26 +19,27 @@ function MeetingTimeDateSelection({ eventInfo, businessInfo }) {
     const [date, setDate] = useState(new Date());
     const [timeSlots, setTimeSlots] = useState([]);
     const [availableTimeSlots, setAvailableTimeSlots] = useState([]);
-    const [selectedTime, setSelectedTime] = useState('');
-    const [userName, setUserName] = useState('');
-    const [userEmail, setUserEmail] = useState('');
+    const [enableTimeSlot, setEnabledTimeSlot] = useState(false);
+    const [selectedTime, setSelectedTime] = useState();
+    const [userName, setUserName] = useState();
+    const [userEmail, setUserEmail] = useState();
     const [userNote, setUserNote] = useState('');
     const [prevBooking, setPrevBooking] = useState([]);
     const [step, setStep] = useState(1);
-    const [loading, setLoading] = useState(false);
     const router = useRouter();
     const db = getFirestore(app);
+    const [loading, setLoading] = useState(false);
     const plunk = new Plunk(process.env.NEXT_PUBLIC_PLUNK_API_KEY);
 
     useEffect(() => {
         if (eventInfo?.duration) {
-            createTimeSlots(eventInfo.duration, date);
+            createTimeSlot(eventInfo.duration, date);
         }
     }, [eventInfo, date]);
 
     useEffect(() => {
-        if (date) {
-            fetchPrevBookings(date);
+        if (date && enableTimeSlot) {
+            getPrevEventBooking(date);
         }
     }, [date]);
 
@@ -45,14 +47,14 @@ function MeetingTimeDateSelection({ eventInfo, businessInfo }) {
         filterAvailableTimeSlots();
     }, [timeSlots, prevBooking]);
 
-    const createTimeSlots = (interval, selectedDate) => {
+    const createTimeSlot = (interval, selectedDate) => {
         const startTime = 8 * 60; // 8 AM in minutes
         const endTime = 22 * 60; // 10 PM in minutes
         const now = new Date();
         let startMinutes = startTime;
 
-        // Adjust start time for today
-        if (selectedDate.getDate() === now.getDate()) {
+        if (format(selectedDate, 'yyyy-MM-dd') === format(now, 'yyyy-MM-dd')) {
+            // If today, start from the next available interval
             const currentMinutes = now.getHours() * 60 + now.getMinutes();
             startMinutes = Math.max(startTime, Math.ceil(currentMinutes / interval) * interval);
         }
@@ -67,28 +69,14 @@ function MeetingTimeDateSelection({ eventInfo, businessInfo }) {
             return `${String(formattedHours).padStart(2, '0')}:${String(minutes).padStart(2, '0')} ${period}`;
         });
 
+        console.log('Created time slots:', slots);
         setTimeSlots(slots);
     };
 
-    const handleDateChange = (selectedDate) => {
-        setDate(selectedDate);
-        createTimeSlots(eventInfo.duration, selectedDate);
-    };
-
-    const fetchPrevBookings = async (selectedDate) => {
-        const q = query(collection(db, 'ScheduledMeetings'),
-            where('selectedDate', '==', selectedDate.toISOString().slice(0, 10)),
-            where('eventId', '==', eventInfo.id));
-
-        const querySnapshot = await getDocs(q);
-        const bookedSlots = querySnapshot.docs.map(doc => doc.data().selectedTime);
-
-        setPrevBooking(bookedSlots);
-    };
-
-    const filterAvailableTimeSlots = () => {
-        const filteredSlots = timeSlots.filter(slot => !prevBooking.includes(slot));
-        setAvailableTimeSlots(filteredSlots);
+    const handleDateChange = (date) => {
+        setDate(date);
+        setEnabledTimeSlot(true);
+        createTimeSlot(eventInfo.duration, date);
     };
 
     const handleScheduleEvent = async () => {
@@ -98,9 +86,9 @@ function MeetingTimeDateSelection({ eventInfo, businessInfo }) {
             return;
         }
 
-        setLoading(true);
         const docId = Date.now().toString();
-        const scheduledMeeting = {
+        setLoading(true);
+        await setDoc(doc(db, 'ScheduledMeetings', docId), {
             businessName: businessInfo.businessName,
             businessEmail: businessInfo.email,
             selectedTime: selectedTime,
@@ -114,28 +102,23 @@ function MeetingTimeDateSelection({ eventInfo, businessInfo }) {
             userName: userName,
             userEmail: userEmail,
             userNote: userNote
-        };
-
-        try {
-            await setDoc(doc(db, 'ScheduledMeetings', docId), scheduledMeeting);
+        }).then(resp => {
             toast('Meeting Scheduled successfully!');
-            sendConfirmationEmail(userName);
-        } catch (error) {
+            sendEmail(userName);
+        }).catch(error => {
             console.error("Error scheduling meeting: ", error);
-            toast('Failed to schedule meeting');
-        } finally {
             setLoading(false);
-        }
+        });
     };
 
-    const sendConfirmationEmail = async (userName) => {
+    const sendEmail = async (user) => {
         const emailHtml = render(<Email
             businessName={businessInfo?.businessName}
-            date={format(date, 'PPP')}
+            date={format(date, 'PPP').toString()}
             duration={eventInfo?.duration}
             meetingTime={selectedTime}
             meetingUrl={eventInfo.locationUrl}
-            userFirstName={userName}
+            userFirstName={user}
         />);
 
         try {
@@ -145,34 +128,63 @@ function MeetingTimeDateSelection({ eventInfo, businessInfo }) {
                 body: emailHtml,
             });
             console.log("Email sent successfully:", response);
-            router.push('/confirmation');
+            setLoading(false);
+            router.replace('/confirmation');
         } catch (error) {
             console.error("Error sending email: ", error);
             toast('Failed to send email notification');
-        } finally {
             setLoading(false);
         }
     };
 
+    const getPrevEventBooking = async (date_) => {
+        const q = query(collection(db, 'ScheduledMeetings'),
+            where('selectedDate', '==', format(date_, 'yyyy-MM-dd')),
+            where('eventId', '==', eventInfo.id));
+
+        const querySnapshot = await getDocs(q);
+        const bookedSlots = [];
+
+        querySnapshot.forEach((doc) => {
+            bookedSlots.push(doc.data().selectedTime);
+        });
+
+        console.log('Previous bookings:', bookedSlots);
+        setPrevBooking(bookedSlots);
+    };
+
+    const filterAvailableTimeSlots = () => {
+        const filteredSlots = timeSlots.filter(slot => !prevBooking.includes(slot));
+        console.log('Available time slots:', filteredSlots);
+        setAvailableTimeSlots(filteredSlots);
+    };
+
     return (
-        <div className='p-5 py-10 shadow-lg m-5 border-t-8 mx-10 md:mx-26 lg:mx-56 my-10'
-            style={{ borderTopColor: eventInfo?.themeColor }}>
-            <Image src='/Logo.png' alt='logo' width={150} height={150} />
+        <div className='p-5 py-10 shadow-lg m-5 border-t-8
+        mx-10
+        md:mx-26
+        lg:mx-56
+        my-10'
+            style={{ borderTopColor: eventInfo?.themeColor }}
+        >
+            <Image src='/Logo.png' alt='logo'
+                width={150}
+                height={150} />
             <div className='grid grid-cols-1 md:grid-cols-3 mt-5'>
                 <div className='p-4 border-r'>
                     <h2>{businessInfo?.businessName}</h2>
-                    <h2 className='font-bold text-3xl'>{eventInfo?.eventName || 'Meeting Name'}</h2>
+                    <h2 className='font-bold text-3xl'>{eventInfo?.eventName ? eventInfo?.eventName : 'Meeting Name'}</h2>
                     <div className='mt-5 flex flex-col gap-4'>
-                        <h2 className='flex gap-2'><Clock />{eventInfo?.duration} Min</h2>
-                        <h2 className='flex gap-2'><MapPin />{eventInfo?.locationType} Meeting</h2>
-                        <h2 className='flex gap-2'><CalendarCheck />{format(date, 'PPP')}</h2>
-                        {selectedTime && <h2 className='flex gap-2'><Timer />{selectedTime}</h2>}
-                        <Link href={eventInfo?.locationUrl || '#'} className='text-primary'>
+                        <h2 className='flex gap-2'><Clock />{eventInfo?.duration} Min </h2>
+                        <h2 className='flex gap-2'><MapPin />{eventInfo?.locationType} Meeting </h2>
+                        <h2 className='flex gap-2'><CalendarCheck />{format(date, 'PPP')}  </h2>
+                        {selectedTime && <h2 className='flex gap-2'><Timer />{selectedTime}  </h2>}
+                        <Link href={eventInfo?.locationUrl ? eventInfo?.locationUrl : '#'} className='text-primary'>
                             {eventInfo?.locationUrl}
                         </Link>
                     </div>
                 </div>
-                {step === 1 ?
+                {step == 1 ?
                     <TimeDateSelection
                         date={date}
                         enableTimeSlot={enableTimeSlot}
@@ -189,12 +201,12 @@ function MeetingTimeDateSelection({ eventInfo, businessInfo }) {
                     />}
             </div>
             <div className='flex gap-3 justify-end'>
-                {step === 2 && <Button variant="outline" onClick={() => setStep(1)}>Back</Button>}
-                {step === 1 ?
+                {step == 2 && <Button variant="outline" onClick={() => setStep(1)}>Back</Button>}
+                {step == 1 ?
                     <Button className="mt-10 float-right"
                         disabled={!selectedTime || !date}
-                        onClick={() => setStep(step + 1)}>
-                        Next
+                        onClick={() => setStep(step + 1)}
+                    >Next
                     </Button> :
                     <Button disabled={!userEmail || !userName} onClick={handleScheduleEvent}>
                         {loading ? <LoaderIcon className='animate-spin' /> : 'Schedule'}
@@ -205,3 +217,4 @@ function MeetingTimeDateSelection({ eventInfo, businessInfo }) {
 }
 
 export default MeetingTimeDateSelection;
+``
